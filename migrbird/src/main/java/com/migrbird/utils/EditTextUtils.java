1201bf32decf3bf7fa3c66219da33a8a70f98d31
alan <alanliu@zego.im>
2022-09-13 15:22:50 +0800

    add regex

--- migrbird/src/main/java/com/migrbird/utils/EditTextUtils.java
+++ migrbird/src/main/java/com/migrbird/utils/EditTextUtils.java
@@ -1,7 +1,9 @@
 package com.migrbird.utils;
 
 import android.text.InputFilter;
+import android.text.Spanned;
 import android.widget.EditText;
+import java.util.regex.Pattern;
 
 public class EditTextUtils {
 
@@ -11,5 +13,23 @@ public class EditTextUtils {
         System.arraycopy(filters, 0, newFilters, 0, filters.length);
         newFilters[filters.length] = filter;
         editText.setFilters(newFilters);
+    }
+
+    public static class EnChNumberLengthFilter extends InputFilter.LengthFilter {
+
+        public EnChNumberLengthFilter(int max) {
+            super(max);
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            CharSequence charSequence = super.filter(source, start, end, dest, dstart, dend);
+            boolean matches = RegexUtils.isMatch(RegexUtils.REGEX_USERNAME, charSequence);
+            if (matches) {
+                return charSequence;
+            } else {
+                return "";
+            }
+        }
     }
 }
